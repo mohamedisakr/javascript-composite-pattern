cc2d353f30e2ca6b569f5dfd4a7dec55137a8efa
mohamedisakr <mohamedisakr@gmail.com>
2021-01-23 18:07:38 +0200

    Using array-like object

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Composite Pattern
+
+1. Use property (an array) to hold collection items
+2. Convert to component function
+3. Convert to class
--- collection.js
+++ collection.js
@@ -7,29 +7,72 @@ const objectsWithValues = [
   { value: 9 },
 ];
 
-function values() {
+function CompositeCollection() {
   this.length = 0;
 }
 
-values.prototype.append = function (item) {
-  if (typeof item === "object" && "value" in item) {
-    this[this.length] = item.value;
-  } else if (typeof item === "number") {
-    this[this.length] = item;
-  }
+CompositeCollection.prototype.append = function (item) {
+  const checkItem = (item) => {
+    if (typeof item === "object" && "value" in item) {
+      this[this.length] = item.value;
+    } else if (typeof item === "number") {
+      this[this.length] = item;
+    }
+  };
+
+  checkItem(item);
   this.length++;
+
   return this;
 };
 
-let myValues = new values();
-for (let prop in objectsWithValues) {
-  //   console.log(objectsWithValues[prop].value);
-  myValues.append(objectsWithValues[prop]);
-}
+CompositeCollection.prototype.increment = function (number) {
+  for (var i = 0; i < this.length; i++) {
+    this[i] += number;
+  }
+  return this;
+};
 
-numberValues.forEach((item) => myValues.append(item));
+CompositeCollection.prototype.getValues = function () {
+  return Object.values(this);
+};
 
-console.log(myValues);
+// 1st example
+let valuesComposition = new CompositeCollection();
+valuesComposition.append(1).append(2).append({ value: 3 }).increment(2);
+console.log(valuesComposition.getValues());
+
+// // 2nd example
+// let MyComposite = new CompositeCollection();
+// for (let key in objectsWithValues) {
+//   //   console.log(objectsWithValues[prop].value);
+//   MyComposite.append(objectsWithValues[key]);
+// }
+
+// numberValues.forEach((item) => MyComposite.append(item));
+// // console.log("Using append WITHOUT using getValues");
+// // console.log(MyComposite);
+
+// /*
+// console.log("Using append WITH using getValues");
+// let coll = MyComposite.getValues();
+// console.log(coll);
+// console.log(coll.join(" "));
+// */
+
+// console.log("Before increment");
+// let coll = MyComposite.getValues();
+// console.log(coll);
+
+// console.log("Using increment");
+// MyComposite.increment(3);
+// coll = MyComposite.getValues();
+// console.log(coll);
+
+// let x = { a: 10, b: 3 };
+// // let keys = Object.keys(x);
+// let vals = Object.values(x);
+// console.log(vals);
 
 /*
 objectsWithValues.forEach((item) => {
